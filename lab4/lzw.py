139cba6bb99cb4aee88046b444d32fd3da7c83ef
TheMAN1003 <fi.03maksimnedozhdii@gmail.com>
2025-10-28 21:32:16 +0200

    init

---
+++ lab4/lzw.py
@@ -0,0 +1,12 @@
+class LZW:
+    def __init__(self, length):
+        self.dictionary = {(i, None): i for i in range(256)}
+        self.S = (-1, -1)
+        self.I = -1
+
+    def encode():
+
+        return
+    
+    def decode():
+        return
